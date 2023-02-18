51f2843941e45ce58ffb7f631c3ce7ab2982da5c
areeb786 <areebrockzz786@gmail.com>
2023-02-18 17:36:21 +0530

    implemented Shimmer Effect

--- app/src/main/java/com/areeb/sekaisheet/ui/Home/pagination/HomePaginationSource.kt
+++ app/src/main/java/com/areeb/sekaisheet/ui/Home/pagination/HomePaginationSource.kt
@@ -4,10 +4,12 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.areeb.sekaisheet.data.models.unsplashModels.WallpaperUnSplashDtoItem
 import com.areeb.sekaisheet.data.network.remote.api.home.HomeApi
+import com.areeb.sekaisheet.utils.Constants.ApiObjects.Companion.TRENDING
+import java.util.*
 
 class HomePaginationSource(
     private val query: String,
-    private val homeApi: HomeApi
+    private val homeApi: HomeApi,
 ) :
     PagingSource<Int, WallpaperUnSplashDtoItem>() {
     override fun getRefreshKey(state: PagingState<Int, WallpaperUnSplashDtoItem>): Int? {
@@ -19,7 +21,7 @@ class HomePaginationSource(
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, WallpaperUnSplashDtoItem> {
         return try {
-            val position = params.key ?: 1
+            val position = params.key ?: if (query == TRENDING) getRandomData() else 1
             val lastPosition = params.loadSize
             val response = homeApi.getWallpaper(query, position, lastPosition)
             return LoadResult.Page(
@@ -27,10 +29,19 @@ class HomePaginationSource(
                 prevKey = if (position == 1) null else position - 1,
                 nextKey = if (response.results.isEmpty()) {
                     null
-                } else position + 1
+                } else {
+                    position + 1
+                },
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
         }
+    }
+
+    fun getRandomData(): Int {
+//        val random = Random()
+//        return random.nextInt(1) + 1
+
+        return 1
     }
 }
